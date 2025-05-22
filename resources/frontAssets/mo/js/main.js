764286f5ad7ccd4aaf833869730063055def4168
lee <elkk20@inpix.com>
2025-05-22 17:13:45 +0900

    top버튼 수정, 메인 아코디언 하단라인 삭제

--- resources/frontAssets/mo/js/main.js
+++ resources/frontAssets/mo/js/main.js
@@ -237,8 +237,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
   if (topButton) {
     window.addEventListener('scroll', function () {
-      if (window.pageYOffset > showOffset) {
+      if (window.scrollY > showOffset) {
         topButton.classList.add('visible');
+
+        const observer = new IntersectionObserver(entries => {
+          entries.forEach(entry => {
+            if (entry.isIntersecting) {
+              topButton.classList.remove('sticky');
+            } else {
+              topButton.classList.add('sticky');
+            }
+          });
+        });
+        observer.observe(topButton);
       } else {
         topButton.classList.remove('visible');
       }
