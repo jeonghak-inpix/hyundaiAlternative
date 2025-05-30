96d56aa3505ffcfebb2ffb8327b15e0a6fc54a73
lee <elkk20@inpix.com>
2025-05-30 10:50:57 +0900

    css 수정

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,16 @@
 const purgecss = require('@fullhuman/postcss-purgecss').default;
 const cssnano = require('cssnano');
-const path = require('path');
 
-module.exports = ctx => {
-  const from = ctx.options.from || '';
-  const normalized = path.normalize(from);
-  const isMobile = normalized.includes(`${path.sep}mo${path.sep}`);
+const isMobile = process.env.POSTCSS_ENV === 'mo';
 
-  return {
-    plugins: [
-      purgecss({
-        content: isMobile
-          ? ['./mo/**/*.html', './resources/frontAssets/mo/js/main.js']
-          : ['./pc/**/*.html', './resources/frontAssets/pc/js/main.js'],
-        defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-      }),
-      cssnano({ preset: 'default' }),
-    ],
-  };
+module.exports = {
+  plugins: [
+    purgecss({
+      content: isMobile
+        ? ['./mo/**/*.html', './resources/frontAssets/mo/js/**/*.js']
+        : ['./pc/**/*.html', './resources/frontAssets/pc/js/**/*.js'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    }),
+    cssnano({ preset: 'default' }),
+  ],
 };
